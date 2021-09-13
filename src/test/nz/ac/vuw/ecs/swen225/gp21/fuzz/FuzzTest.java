570e3f6e7c81fec1e495521a25d860d21b6d94cf
HARPER DOAK <harperdoak@HARPERs-MacBook-Pro.local>
2021-09-13 16:06:46 +1200

    Added FuzzTest Class

---
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
@@ -0,0 +1,16 @@
+package test.nz.ac.vuw.ecs.swen225.gp21.fuzz;
+
+
+public class FuzzTest {
+	
+	//Test1 is testing the first level of the game
+	public void test1() {
+		
+	}
+	
+	//Test2 is testing the second level of the game
+	public void test2() {
+		
+	}
+
+}
