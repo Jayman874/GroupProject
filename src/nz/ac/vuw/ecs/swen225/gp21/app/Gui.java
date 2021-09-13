a3d080cfd02f4b5a7a85d22ab9d06f3af5d062fc
Harper Doak <doakharp@ecs.vuw.ac.nz>
2021-09-13 21:04:58 +1200

    GUI Class Made

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/Gui.java
@@ -0,0 +1,11 @@
+package nz.ac.vuw.ecs.swen225.gp21.app;
+
+import javax.swing.*;
+
+public class Gui {
+	
+	public static void main(String args[]) {
+		JFrame frame = new JFrame("Chip Vs Chap");
+	}
+
+}
