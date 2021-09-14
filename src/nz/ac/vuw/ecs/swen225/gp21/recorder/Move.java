ca7e0f8d986f180d9623374ca15518166779d590
gazza177 <gazzadamazza@gmail.com>
2021-09-14 19:57:49 +1200

    Created move class for use within Recorder package and set up the Recorder class

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/recorder/Move.java
@@ -0,0 +1,53 @@
+package src.nz.ac.vuw.ecs.swen225.gp21.recorder;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
+import src.nz.ac.vuw.ecs.swen225.gp21.domain.*;
+
+/**
+ * Move class containing a particular cell and the tile contained within before and after a move is made
+ */
+public class Move {
+
+    private int x;
+    private int y;
+    private Tile beforeMoveTile;
+    private Tile afterMoveTile;
+
+    public Move(int x, int y, Tile currentTile, Tile afterMoveTile) {
+        this.x = x;
+        this.y = y;
+        this.beforeMoveTile = currentTile;
+        this.afterMoveTile = afterMoveTile;
+    }
+
+    /**
+     * Returns the x value.
+     * @return  x integer
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y value.
+     * @return  y integer
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Returns the Tile located within the cell before the move.
+     * @return  before move Tile
+     */
+    public Tile getAfterMoveTile() {
+        return afterMoveTile;
+    }
+
+    /**
+     * Returns the Tile located within the cell before the move.
+     * @return  after move Tile
+     */
+    public Tile getBeforeMoveTile() {
+        return beforeMoveTile;
+    }
+}
--- src/nz/ac/vuw/ecs/swen225/gp21/recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/recorder/Recorder.java
@@ -1,6 +1,55 @@
-package nz.ac.vuw.ecs.swen225.gp21.recorder;
+package src.nz.ac.vuw.ecs.swen225.gp21.recorder;
 
 
+import java.io.File;
+import java.util.Stack;
+
+/**
+ * Recorder class to handle the recording of a game
+ */
 public class Recorder {
+
+    private File saveFile; // xml file to write the recorded game to
+    private Stack<Move> moveStack; // stack to store the current moves made in game
+
+    public Recorder () {
+        this.saveFile = new File("");
+        this.moveStack = new Stack<>();
+    }
+
+    /**
+     * Main method to write run through the stack and write the contents to the xml save file
+     */
+    public void recordCurrentTurn() {
+
+        for(Move move : moveStack) {
+
+        }
+
+    }
+
+    /**
+     * Pushes a move to the moveStack
+     * @param move  Move to be added to stack
+     */
+    public void addMove(Move move) {
+        this.moveStack.push(move);
+    }
+
+    /**
+     * Returns the file the data is being written to.
+     * @return  save File
+     */
+    public File getSaveFile() {
+        return saveFile;
+    }
+
+    /**
+     * Returns the stack where Moves are being stored
+     * @return Stack of Moves
+     */
+    public Stack<Move> getMoveStack() {
+        return moveStack;
+    }
 
 }
