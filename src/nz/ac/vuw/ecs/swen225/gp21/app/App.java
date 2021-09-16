43c51950f09d395e48111611dfca1f59b9d61244
Louis Cooper <louistobiascooper@gmail.com>
2021-09-16 14:21:55 +1200

    input map start

--- src/nz/ac/vuw/ecs/swen225/gp21/app/App.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/App.java
@@ -2,7 +2,11 @@ package nz.ac.vuw.ecs.swen225.gp21.app;
 
 import nz.ac.vuw.ecs.swen225.gp21.domain.*;
 
+import javax.swing.*;
+
 public class App {
+    InputMap inputs = new InputMap();
+
     Tile[][] boar;
 
 
