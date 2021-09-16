fa721d368a3d9b809c4fd559470f39784755dda3
Louis Cooper <louistobiascooper@gmail.com>
2021-09-16 14:56:54 +1200

    added key listeners and skeleton methods

--- src/nz/ac/vuw/ecs/swen225/gp21/app/KeyPress.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/KeyPress.java
@@ -32,27 +32,27 @@ public class KeyPress extends Frame{
         public void keyPressed(KeyEvent ke){
             char i = ke.getKeyChar();
             String str = Character.toString(i);
-            if(str == ("CTRL-X")){
+            if(str.equals("CTRL-X")){
                 //exit game, don't save state
-            } else if(str == ("CTRL-S")){
+            } else if(str.equals("CTRL-S")){
                 //exit game, save and start from this save next time app is opened
-            } else if(str == ("CTRL-R")){
+            } else if(str.equals("CTRL-R")){
                 //resume a saved game by having a pop up a file selector to select a saved game to be loaded
-            } else if(str == ("CTRL-1")){
+            } else if(str.equals("CTRL-1")){
                 //start level 1
-            } else if(str == ("CTRL-2")){
+            } else if(str.equals("CTRL-2")){
                 //start level 2
-            } else if(str == ("SPACE")){
+            } else if(str.equals("SPACE")){
                 //exit game
-            } else if(str == ("ESC")){
+            } else if(str.equals("ESC")){
                 //exit game
-            } else if(str == ("UP")){
+            } else if(str.equals("UP")){
                 //exit game
-            } else if(str == ("LEFT")){
+            } else if(str.equals("LEFT")){
                 //exit game
-            } else if(str == ("RIGHT")){
+            } else if(str.equals("RIGHT")){
                 //exit game
-            } else if(str == ("DOWN")){
+            } else if(str.equals("DOWN")){
                 //exit game
             }
         }
