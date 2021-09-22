c8e8ae0f177f36cc24186be841e8317fa07caaad
gulabjaye <gulabjaye@myvuw.ac.nz>
2021-09-22 16:49:39 +1200

    Added layour for suite of Domain JUnit tests

---
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/domain/DomainTests.java
@@ -0,0 +1,34 @@
+package test.nz.ac.vuw.ecs.swen225.gp21.domain;
+
+import org.junit.Test;
+
+
+/**
+ * Suite of tests for the domain package. 
+ * 
+ * @author Jayen
+ *
+ */
+public class DomainTests {
+  
+  @Test
+  public void test1() {
+    
+  }
+  
+  @Test
+  public void test2() {
+    
+  }
+  
+  @Test
+  public void test3() {
+    
+  }
+  
+  @Test
+  public void test4() {
+    
+  }
+  
+}
