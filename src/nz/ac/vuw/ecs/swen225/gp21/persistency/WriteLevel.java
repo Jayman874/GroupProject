b5555b4afd6827e884d5cc87cc7fe34116b435d6
Edward Horwood <horwooedwa@myvuw.ac.nz>
2021-09-30 19:14:27 +1300

    Finished the logic for the actors

--- src/nz/ac/vuw/ecs/swen225/gp21/persistency/WriteLevel.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistency/WriteLevel.java
@@ -14,8 +14,7 @@ import java.util.List;
 public class WriteLevel {
     public static void main(String[] args) {
         WriteLevel main = new WriteLevel();
-        main.createLevel("testingActor.xml", "f", 5);
-        editCellType("testingActor.xml", 0, 0, "a");
+        editCellType("level1.xml", 0, 3, "a");
 
     }
 
--- src/nz/ac/vuw/ecs/swen225/gp21/persistency/levels/level2/Actor.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistency/levels/level2/Actor.java
@@ -1,8 +1,6 @@
 package nz.ac.vuw.ecs.swen225.gp21.persistency.levels.level2;
 
-import nz.ac.vuw.ecs.swen225.gp21.domain.Board;
-import nz.ac.vuw.ecs.swen225.gp21.domain.Location;
-import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
+import nz.ac.vuw.ecs.swen225.gp21.domain.*;
 
 import java.util.List;
 import java.util.Random;
@@ -18,9 +16,10 @@ public class Actor implements Tile {
         for(int i = 0; i < actors.size(); i++){
             Actor a = actors.get(i);
             Location newLoc = a.getNewLocation();
-            Board.updateActorBoard(a, newLoc);
+            if(a.isValid(newLoc)){
+                Board.updateActorBoard(a, newLoc);
+            }
         }
-
     }
 
     public Location getNewLocation(){
@@ -28,22 +27,19 @@ public class Actor implements Tile {
         int x = location.getX();
         int y = location.getY();
 
-        if(random == 1){
-            x++;
-            y++;
-        }else if(random == 2){
-            x++;
-            y--;
-        }else if(random == 3){
-            x--;
-            y++;
-        }else if(random == 4){
-            x--;
-            y--;
-        }
-        return new Location(x, y);
+        if(random == 1 && (x + 1 < 19)){ x++; }
+        else if(random == 2 && (y - 1 > 0)){ y--; }
+        else if(random == 3 && (x - 1 > 0)){ x--; }
+        else if(random == 4 && (y + 1 < 19)){ y++; }
+        Location newLoc = new Location(x, y);
+        return newLoc;
+
     }
 
+    /**
+     *
+     * @return random number representing a directino for the actor to move
+     */
     public int getRandom(){
         Random rand = new Random();
         int max = 4;
@@ -55,7 +51,33 @@ public class Actor implements Tile {
 
     @Override
     public boolean isValid(Location loc) {
+        int x = loc.getX();
+        int y = loc.getY();
+        Tile[][] board = Board.getBoard();
+        Tile tile = board[y][x];
+        if(actorMoveCheck(board, x, y)){
+            if (tile instanceof FreeTile) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public boolean actorMoveCheck(Tile[][] board, int x, int y){
+        int actorX = this.getLocation().getX();
+        int actorY = this.getLocation().getY();
+        // gets the absolute difference of move
+        int xlocDifference = Math.abs(x - actorX);
+        int ylocDifference = Math.abs(y - actorY);
+        // checks if chap tries to move out of game board
+        if (x < 0 || y < 0 || x > board.length - 1 || y > board.length - 1) {
+            return false;
+        }
+        // checks if chap tries to move more than one space at a time
+        if (xlocDifference > 1 || ylocDifference > 1) {
+            return false;
+        }
+        return true;
     }
 
     @Override
