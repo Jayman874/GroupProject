ebbf22d048753eccd952bc135bc3fe8fe90f1327
Harper Doak <doakharp@ecs.vuw.ac.nz>
2021-09-30 20:24:41 +1300

    Added a valid move test to fuzz tests. Brings domain module coverage to 90%

--- src/test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
@@ -1,7 +1,10 @@
 package test.nz.ac.vuw.ecs.swen225.gp21.fuzz;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
 import nz.ac.vuw.ecs.swen225.gp21.app.App;
+import nz.ac.vuw.ecs.swen225.gp21.domain.*;
 import org.junit.*;
 import org.junit.Test;
 //import org.junit.jupiter.api.Test;
@@ -10,8 +13,28 @@ public class FuzzTest {
 
 	@Test
 	public void test1() {
-		///new App();
-		//assert true;
+
+		//valid move
+		new Board();
+		Chap chap = new Chap();
+		Tile[][] tile = Board.getBoard();
+		FreeTile freeTile = new FreeTile();
+		freeTile.getLocation();
+		freeTile.toString();
+		tile[0][0] = chap;
+		tile[0][1] = freeTile;
+		Location chapLoc = new Location(0, 0);
+		Location freeLoc = new Location(1, 0);
+		chap.setLocation(chapLoc);
+		freeTile.setLocation(freeLoc);
+		freeTile.setLocation(freeLoc);
+		if ((chap.isValid(freeLoc))) {
+			Board.updateBoard(chap, freeLoc);
+			assertTrue(true);
+		} else {
+			fail("Chap cannot move into this tile");
+		}
+
 	}
 	
 	public void test2() {
