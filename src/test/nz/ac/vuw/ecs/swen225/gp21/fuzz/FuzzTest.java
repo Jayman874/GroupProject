5f46256049147a9cb686060993317aeff9e26644
Louis Cooper <louistobiascooper@gmail.com>
2021-10-01 16:45:33 +1300

    added fuzz randomness

--- src/test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
@@ -18,30 +18,21 @@ public class FuzzTest {
 		FreeTile freeTile = new FreeTile();
 		freeTile.getLocation();
 		freeTile.toString();
+		double a = Math.random() * 2;
+		double b = Math.random() * 2;
 		tile[0][0] = chap;
 		tile[0][1] = freeTile;
 		Location chapLoc = new Location(0, 0);
-		Location freeLoc = new Location(1, 0);
+		Location freeLoc = new Location((int)a, (int)b);
 		chap.setLocation(chapLoc);
 		freeTile.setLocation(freeLoc);
 		freeTile.setLocation(freeLoc);
 		if ((chap.isValid(freeLoc))) {
+			System.out.println(a + " " + b);
 			Board.updateBoard(chap, freeLoc);
 			assertTrue(true);
 		} else {
 			fail("Chap cannot move into this tile");
 		}
-	}
-	
-	public void test2() {
-		
-	}
-	
-	public void test3() {
-		
-	}
-	
-	public void test4() {
-		
 	}
 }
